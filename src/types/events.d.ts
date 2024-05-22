97fb6ea9a08d7b8d411cbc3d615f996a8149a168
AntanasGa <anatanas.ga@gmail.com>
2024-05-22 13:04:44 +0300

    correcting missing definition

--- src/types/events.d.ts
+++ src/types/events.d.ts
@@ -549,4 +549,5 @@ export type EventItem = ChannelFollowSubscription
 | ChannelChatClearSubscription
 | ChannelChatClearUserMessageSubscription
 | ChannelChatMessageSubscription
-| ChannelChatMessageDeleteSubscription;
+| ChannelChatMessageDeleteSubscription
+| ChannelChatNotificationEvent;
